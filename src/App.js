4731d3283650dc67e7a10acbbf69e373c85470f2
User-s <ani.gamcemlidze20@gmail.com>
2024-03-08 11:35:32 +0400

    Install Tailwind, add header to the project, and start creating main content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import logo from "./logo.svg";
+import logo from "./images/X-Logo.png";
+import content_image from "./images/veggies.jpg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React!</p>
+    <div className="main-container">
+      <header className="flex justify-between items-center p-4 bg-slate-500	">
+        <img alt="X-logo" className="w-6 h-6 object-contain" src={logo} />
+        <nav>
+          <ul className="flex">
+            <li>
+              <button className="pr-2	">Home</button>
+            </li>
+            <li>
+              <button className="pr-2	">About</button>
+            </li>
+            <li>
+              <button className="pr-2	">Product</button>
+            </li>
+            <li>
+              <button className="pr-2	">Blog</button>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main
+        className="bg-cover bg-no-repeat bg-gradient-to-l	"
+        style={{ backgroundImage: `url(${content_image})`, height: "496px" }}
+      >
+        <div className="absolute top-0 bg-black"></div>
+      </main>
     </div>
   );
 }
