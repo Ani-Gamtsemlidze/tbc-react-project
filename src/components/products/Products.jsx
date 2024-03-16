fee60685baf1fdd7fccbdeb1638ac7c0167b19eb
Ani Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2024-03-16 19:02:12 +0100

    add cart button to the products list

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -6,7 +6,8 @@ export default function Products({title, description,img}) {
 
         <h1 className='text-xl font-bold text-center mt-2'>{title}</h1>
         <img className='w-64 my-4  h-48 object-cover' src={img} />
-        <p className='text-base w-56 p-4'>{description}</p>
+        <p className=' text-gray-950 text-sm     w-56 p-4'>{description}</p>
+        <button className='bg-slate-300  rounded px-2 py-1 flex mt-2 mx-auto mb-2 cursor-pointer hover:bg-white transition'>Add to Cart</button>
         </div>
     </div>
   )
