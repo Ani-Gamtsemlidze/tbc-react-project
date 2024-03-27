8f7985f76a077ff1faa5ba0ae4d263075bd0c508
Ani Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2024-03-27 11:58:37 +0100

    Style: Apply design to the blog page

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -3,14 +3,25 @@ import blogData from '../../blogData.json';
 
 function Blog() {
   return (
-    <div className='my-8 flex flex-1 flex-wrap px-4 justify-center'>
-        {blogData.map((blog) => (
-            <div className='flex flex-col grow-0 shrink-0 basis-1/4 m-2 bg-slate-600 p-4 rounded'>
-                <h1 className='text-lg'>{blog.title}</h1>
-                <img className='w-96 h-40 object-cover my-6' src={blog.imageSrc} />
-                <p className='mb-4'>{blog.description}</p>
-                <span>{blog.publishDate}</span>
+    <div className=' flex flex-1 flex-wrap p-4 justify-start bg-slate-500 '>
+        {blogData.map((blog) => (   
+            <div key={blog.id} className=' border-2 flex flex-col grow-0 shrink-0 basis-[22%] m-4 p-4 rounded '>
+                <div className='min-h-16'>
 
+                <h1 className='text-lg text-center'>{blog.title}</h1>
+                </div>
+                <img className='w-96 h-40 object-cover my-6' src={blog.imageSrc} />
+                <div className='min-h-32'>
+
+                <p className='mb-4'>{blog.description}</p>
+                </div>
+
+                <div className='min-h-16'>
+
+                <span>{blog.publishDate}</span>
+                </div>
+
+                <button className='mt-8 border w-36 mx-auto p-2 hover:bg-white  transition'>View More</button>
             </div>
 
         ))}
