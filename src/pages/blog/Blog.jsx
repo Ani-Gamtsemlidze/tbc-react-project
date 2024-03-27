0c42eb11062da15ba0d316e50c8bdca71dffecf6
Ani Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2024-03-27 12:07:02 +0100

    Style: make changes in style

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -5,13 +5,13 @@ function Blog() {
   return (
     <div className=' flex flex-1 flex-wrap p-4 justify-start bg-slate-500 '>
         {blogData.map((blog) => (   
-            <div key={blog.id} className=' border-2 flex flex-col grow-0 shrink-0 basis-[22%] m-4 p-4 rounded '>
+            <div key={blog.id} className=' border-2 flex flex-col grow-0 shrink-0 basis-[22%] m-4 px-6  rounded '>
+                <img className='w-96 h-40 object-cover my-4' src={blog.imageSrc} />
                 <div className='min-h-16'>
 
-                <h1 className='text-lg text-center'>{blog.title}</h1>
+                <h1 className='text-lg text-left'>{blog.title}</h1>
                 </div>
-                <img className='w-96 h-40 object-cover my-6' src={blog.imageSrc} />
-                <div className='min-h-32'>
+                <div className='min-h-32 mt-2'>
 
                 <p className='mb-4'>{blog.description}</p>
                 </div>
@@ -21,7 +21,7 @@ function Blog() {
                 <span>{blog.publishDate}</span>
                 </div>
 
-                <button className='mt-8 border w-36 mx-auto p-2 hover:bg-white  transition'>View More</button>
+                <button className='my-8 border w-36 mx-auto p-2 hover:bg-white  transition'>View More</button>
             </div>
 
         ))}
