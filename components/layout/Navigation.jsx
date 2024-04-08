0c5cd1071bcf6f0caff31de2aba2349777c8f682
Ani Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2024-04-08 15:28:30 +0200

    Navigation: Add active link by using pathname

--- components/layout/Navigation.jsx
+++ components/layout/Navigation.jsx
@@ -1,6 +1,11 @@
+"use client"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+
 
 export default function Navigation() {
+  const pathname = usePathname()
+  console.log(pathname)
     const navigation = [{title:"Home", href:"/"}, {title:"Profile", href:"profile"},
     {title:"Contact", href:"contact"}, {title:"Blog", href:"blog"}]
     
@@ -9,7 +14,11 @@ export default function Navigation() {
    <ul className="flex mr-8 my-3">
      {navigation.map((list) => (
        <li key={list.href}>
-         <Link className="mr-4 text-white hover:border-b hover:border-white" 
+         <Link className={`mr-4 text-white hover:border-b pb-1.5 hover:border-white ${
+    pathname === (list.href === "/" ? "/" : `/${list.href}`)
+      ? "active"
+      : ""
+  }`}   
          href={list.href}>{list.title}</Link>
        </li>
      ))}
