2f6ec0e7d167a8d6c34a72a9cfcc6ec66106dd65
Ani Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2024-04-20 17:06:29 +0200

    Blog Page: start set up generate static params in blog page

--- app/blog/[blogId]/page.jsx
+++ app/blog/[blogId]/page.jsx
@@ -1,7 +1,18 @@
 import InnerBlog from "@/components/blog/InnerBlog";
-import Image from "next/image";
+
+export async function generateStaticParams() {
+  const posts = await fetch("https://dummyjson.com/recipes").then((res) =>
+    res.json()
+  );
+
+  return posts.map((post) => ({
+    blogId: post.blogId,
+  }));
+}
+
 async function blogsInnerData({ params }) {
-  const res = await fetch(`https://dummyjson.com/recipes/${params.blogId}`);
+  const { blogId } = params;
+  const res = await fetch(`https://dummyjson.com/recipes/${blogId}`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -12,8 +23,5 @@ async function blogsInnerData({ params }) {
 export default async function Page({ params }) {
   const blogsData = await blogsInnerData({ params });
 
-  return (
-    <InnerBlog blogsData={blogsData} />
-
-  );
+  return <InnerBlog blogsData={blogsData} />;
 }
