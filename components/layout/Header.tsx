9150a4a74dbad56d47450adc07d77dedb91a7ac7
Ani Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2024-04-30 23:40:16 +0200

    Change Logo based on dark mode

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,11 +1,25 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import Navigation from "./Navigation";
 import LogoutBtn from "../logout/LogoutBtn";
 import LocalSwitcher from "../langSwitcher/LocalSwitcher";
 import ThemeSwitch from "../theme/ThemeSwitch";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const { theme } = useTheme();
+  const [isDark, setIsDark] = useState(true);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      setIsDark(false);
+    } else {
+      setIsDark(true);
+    }
+  }, [theme]);
+
   return (
     <>
       <header className="flex  justify-between items-center bg-[#F6F3EC] dark:bg-slate-600 py-1 px-8">
@@ -14,10 +28,7 @@ export default function Header() {
             <Image
               alt="X-logo"
               className="w-16 h-16 object-contain mr-4"
-              src={
-                "/images/logo.png"
-                //  "/images/light-logo.png"
-              }
+              src={isDark ? "/images/logo.png" : "/images/light-logo.png"}
               width={400}
               height={400}
             />
--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -14,14 +14,24 @@ export default function Navigation() {
   ];
 
   return (
-    <nav>
-      <ul className="flex mr-8 my-3">
+    <nav className="fill ">
+      <ul className=" flex mr-8 my-3">
         {navigation.map((list) => (
           <li key={list.href}>
             <Link
-              className={`text-[4C4C4C] mr-4 text-dm transition rounded dark:text-white text-lg ${
-                pathname === list.href ? "active" : ""
-              }`}
+              className={`  mr-4 rounded dark:text-white text-lg
+              block py-2 px-3 font-bold my-2 uppercase relative
+
+              after:content-['.'] after:text-left after:opacity-5 after:m-0 
+              after:absolute after:mx-auto after:text-transparent after:inset-x-0 after:bottom-0	after:w-0  after:bg-[#aaa] after:h-[1px]
+             transition-all 
+
+              duration-[0.5s]
+
+              hover:text-white hover:z-10 dark:hover:text-[#E2E8F0]
+
+              
+              ${pathname === list.href ? "active" : ""}`}
               href={list.href}
             >
               {list.title}
