2cad88e9ec75ae8bf185c8cde782107ad9b885e2
Ani Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2024-06-04 13:32:10 +0200

    Update auth0 login code, Save users during login/signup in the database

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,4 +1,14 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
 
   
-export const GET = handleAuth();
+export const GET = handleAuth({
+    login: handleLogin({
+      returnTo: "/api/handle-login",
+      authorizationParams: {
+        prompt: "login" 
+      }
+    }),
+    logout: handleLogout({
+      returnTo: "/" 
+    })
+  });
--- app/api/handle-login/route.ts
+++ app/api/handle-login/route.ts
@@ -1,39 +1,32 @@
 import { getSession } from "@auth0/nextjs-auth0";
 import { sql } from "@vercel/postgres";
-import { NextResponse } from "next/server";
+import { redirect } from "next/navigation";
 
 export async function GET(_request: Request) {
   try {
-    const {user}:any = await getSession();
+    const session = await getSession();
 
-    console.log(user, "userrrr")
 
-    if (!user.sub) {
-      throw new Error("sub, email, picture are required");
-    }
+    if (session?.user) {
+      const {email, sub, picture} = session.user
+      const existingUser = await sql`SELECT * FROM users_info WHERE user_id = ${sub};`;
 
-    const existingUser = await sql`SELECT * FROM users_info WHERE sub = ${user.sub};`;
-
-    if (existingUser && existingUser.rows.length > 0) {
-      return NextResponse.json(
-        { error: "sub already exists" },
-        { status: 400 }
-      );
-    } else {
+      if(!existingUser.rows.length)
       await sql`
-      INSERT INTO users_info (sub, email, picture, email_verified, family_name,given_name,nickname, sid)
-      VALUES (${user.sub}, ${user.email}, ${user.picture}, ${user.email_verified}, ${user.family_name}, ${user.given_name}, ${user.nickname}, ${user.sid} );
+      INSERT INTO users_info (user_id, email, picture)
+      VALUES (${sub}, ${email}, ${picture} );
     `;
+
+    } else {
+      return redirect("/api/auth/login")
+    } 
+  }
+    catch (error) {
+      return redirect("/api/auth/logout")
+    }
+    return redirect("/")
+
+
     }
 
-    const singleUser = await sql`SELECT picture FROM users_info WHERE sub = ${user.sub};`;
-
-    console.log(singleUser, "singleUser")
-    
-    return NextResponse.json({ singleUser }, { status: 200 });
-  } catch (error: any) {
-    console.error("Error:", error.message);
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-}
   
