162ab757ba04f1a4bfaa6e62f677e2b2703cccb2
Ani Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2024-06-08 10:56:17 +0200

    Check vercel error

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -2,13 +2,13 @@ import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
 
   
 export const GET = handleAuth({
-    login: handleLogin({
-      returnTo: "/api/handle-login",
-      authorizationParams: {
-        prompt: "login" 
-      }
-    }),
-    logout: handleLogout({
-      returnTo: "/" 
-    })
-  });
+  login: handleLogin({
+    returnTo: "/api/handle-login",
+    authorizationParams: {
+      prompt: "login" 
+    }
+  }),
+  logout: handleLogout({
+    returnTo: "/" 
+  })
+});
